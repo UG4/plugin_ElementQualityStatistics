071ebb158d50eb28f18a8515f1e4b62ae53a76d2
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2011-10-26 11:56:26 +0000

    added element quality statistics (only a stub)

---
+++ plugin_main.cpp
@@ -0,0 +1,25 @@
+// created by Sebastian Reiter
+// s.b.reiter@googlemail.com
+// 12.09.2011 (m,d,y)
+
+#include "registry/registry.h"
+#include "common/ug_config.h"
+#include "common/error.h"
+#include "lib_disc/domain.h"
+#include "lib_grid/lib_grid.h"
+
+#include <string>
+
+using namespace std;
+
+int SayHello()
+{
+	UG_LOG("HELLO From Grid Statistics!\n");
+}
+
+extern "C" UG_API void InitUGPlugin(ug::bridge::Registry* reg, string parentGroup)
+{
+	string grp(parentGroup); grp.append("ElementQualityStatistics/");
+
+	reg->add_function("SayHello", &SayHello, grp);
+}
