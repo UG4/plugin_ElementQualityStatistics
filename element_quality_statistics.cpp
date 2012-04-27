4fd96b1008632a38c97d8c56871c16435aa51352
Martin Stepniewski <martin.stepniewski@gcsc.uni-frankfurt.de>
2012-04-27 08:39:58 +0000

    Minor coding style changes.

--- element_quality_statistics.cpp
+++ element_quality_statistics.cpp
@@ -10,9 +10,14 @@
 
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
 	#include <functional>
-	  template <class T> function<bool(T)> isInRange(T low, T high) {
-      return [low,high](T value) { return std::fabs(value - low) >= std::numeric_limits<T>::epsilon() \
-                                       && std::fabs(value - high) <= std::numeric_limits<T>::epsilon(); }; }
+	  template <class T> function<bool(T)> isInRange(T low, T high)
+	  {
+		  return [low,high](T value)
+				  {
+			  	  	  return std::fabs(value - low) >= std::numeric_limits<T>::epsilon() \
+                             && std::fabs(value - high) <= std::numeric_limits<T>::epsilon();
+				  };
+	  }
 #endif
 
 namespace ug
@@ -553,15 +558,19 @@ void MinVolumeAngleHistogram(Grid& grid)
 		number MinVolumeAngle = MinVolumeAngles[i];
 
 		#ifdef __GXX_EXPERIMENTAL_CXX0X__
-			for (uint range = 0; range < 18; range++) {
-				if (isInRange(range*10, (range+1)*10)(MinVolumeAngle)) {
+			for (uint range = 0; range < 18; range++)
+			{
+				if (isInRange(range*10, (range+1)*10)(MinVolumeAngle))
+				{
 					++counter[range];
 					break;
 				}
 			}
 		#else
-			for (uint range = 0; range < 18; range++) {
-				if (MinVolumeAngle < (range+1)*10) {
+			for (uint range = 0; range < 18; range++)
+			{
+				if (MinVolumeAngle < (range+1)*10)
+				{
 					++counter[range];
 					break;
 				}
